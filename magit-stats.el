99cbfc4ef6f3eee76af9e680847c383659e5cdcc
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2023-02-11 00:51:16 -0500

    improvements
    Use call-process instead of shell commands.
    Prompt for repository when calling commands interactively.
    Give generated buffers unique names.
    Enable output appropriate major modes in generated buffers.
    magit-stats: prompt for repository when passed a prefix-arg.

--- magit-stats.el
+++ magit-stats.el
@@ -40,55 +40,75 @@
 Each entry is of form: (CHAR FN DESCRIPTION)"
   :type 'list)
 
+(defvar magit-stats--output-buffer "*magit-stats-output*")
+
+(defun magit-stats--call (&rest options)
+  "Call CLI with command line OPTIONS.
+Signal an error if command does not exit with status 0."
+  (unless (zerop
+           (apply #'call-process
+                  `("npx" nil ,magit-stats--output-buffer nil "magit-stats" ,@options)))
+    (user-error "%s" (with-current-buffer magit-stats--output-buffer
+                       (prog1 (string-trim (buffer-string)) (kill-buffer))))))
+
+(defmacro magit-stats--with-repo (repo &rest body)
+  "Execute BODY with `default-directory' bound to REPO."
+  (declare (indent 1))
+  `(let ((default-directory (file-name-as-directory (expand-file-name ,repo))))
+     (message "Loading...")
+     ,@body
+     (message "Loaded...")))
 
 ;;;###autoload
-(defun magit-stats-in-buffer ()
-  "HTML report in a new buffer."
-  (interactive)
-  (with-current-buffer (get-buffer-create "*magit-stats-stdout*")
-    (message "Loading...")
-    (erase-buffer)
-    (shell-command "npx magit-stats --html --stdout" (current-buffer))
-    (shr-render-buffer (current-buffer))
-    (pop-to-buffer (current-buffer))
-    (kill-buffer "*magit-stats-stdout*")
-    (message "Loaded!")))
-
+(defun magit-stats-in-buffer (repository)
+  "Display HTML report for REPOSITORY in a new buffer."
+  (interactive "DRepository: ")
+  (magit-stats--with-repo repository
+    (magit-stats--call "--html" "--stdout")
+    (shr-render-buffer magit-stats--output-buffer)
+    (rename-buffer (format "*magit-stats: %s" repository) t)
+    (special-mode)
+    (kill-buffer magit-stats--output-buffer)))
 
 ;;;###autoload
-(defun magit-stats-with-viewer ()
-  "Open HTML report with OS default viewer."
-  (interactive)
-  (message "Loading...")
-  (shell-command  "npx magit-stats")
-  (message "Loaded!"))
-  
+(defun magit-stats-with-viewer (repository)
+  "Open HTML report for REPOSITORY with OS default viewer."
+  (interactive "DRepository: ")
+  (magit-stats--with-repo repository (magit-stats--call)))
 
 ;;;###autoload
-(defun magit-stats-json-buffer ()
-  "JSON report data in a new buffer."
-  (interactive)
-  (with-current-buffer (get-buffer-create "*magit-stats-stdout*")
-    (message "Loading...")
-    (erase-buffer)
-    (shell-command "npx magit-stats --json --stdout" (current-buffer))
-    (pop-to-buffer (current-buffer))
-    (message "Loaded!")))
+(defun magit-stats-json-buffer (repository)
+  "Display JSON report data for REPOSITORY in a new buffer."
+  (interactive "DRepository: ")
+  (magit-stats--with-repo repository
+    (magit-stats--call "--json" "--stdout")
+    (pop-to-buffer magit-stats--output-buffer)
+    (js-json-mode)
+    (rename-buffer (format "*magit-stats: %s" repository) t)))
 
+(defun magit-stats--read-backend ()
+  "Return backend command from `magit-stats-backends'."
+  (car (alist-get (read-char-choice
+                   (format "magit-stats backend (%s):\n%s"
+                           (substitute-command-keys "\\[keyboard-quit] to quit")
+                           (mapconcat (lambda (b) (format "%c - %s\n"
+                                                          (car b)
+                                                          (car (last b))))
+                                      magit-stats-backends))
+                   (mapcar #'car magit-stats-backends))
+                  magit-stats-backends)))
 
 ;;;###autoload
-(defun magit-stats (backend)
-  "Generate GIT Repository Statistics via BACKEND."
-  (interactive
-   (list (car (alist-get (read-char-choice
-                          (format "magit-stats backend (%s):\n%s"
-                                  (substitute-command-keys "\\[keyboard-quit] to quit")
-                                  (mapconcat (lambda (b) (format "%c - %s\n" (car b) (car (last b))))
-                                             magit-stats-backends))
-                          (mapcar #'car magit-stats-backends))
-                         magit-stats-backends))))
-  (if (functionp backend) (funcall backend) (user-error "Unknown backend: %s" backend)))
+(defun magit-stats (repository backend)
+  "Generate GIT REPOSITORY statistics via BACKEND.
+When called interactively with a prefix arg, prompt for REPOSITORY.
+Otherwise default to `default-directory'."
+  (interactive (list (if current-prefix-arg
+                         (read-directory-name "Directory: ")
+                       default-directory)
+                     (magit-stats--read-backend)))
+  (unless (functionp backend) (user-error "Unknown backend: %s" backend))
+  (funcall backend repository))
 
 (provide 'magit-stats)
-
 ;;; magit-stats.el ends here
